99658f8f090faba6e3ef81b4da4c693cb39fb610
Daniel <danieldabic@wp.pl>
2022-09-12 15:20:20 +0200

    Post-processing autoprefixer setup

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 
 gulp.task('watch', watch);
 
 function buildStyles() {
+  const plugins = [
+    autoprefixer()
+  ];
+
   return gulp.src('./src/**/*.scss')
   .pipe(sassGlob())
   .pipe(sass().on('error', sass.logError))
+  .pipe(postcss(plugins))
   .pipe(gulp.dest('./public/css'))
 }
 function watch() {
