e92cc926f65df03a5f991cf42c78ab47b25c1613
Daniel <danieldabic@wp.pl>
2022-10-03 14:21:53 +0200

    skills component

---
+++ frontend/src/components/skills/skillsData.js
@@ -0,0 +1,22 @@
+const data = [{
+  name: 'Soft',
+  list: [
+    'Good communication skills',
+    'Ability to work under pressure of time',
+    'Teamwork',
+    'Easy to get involved',
+    'Creativity',
+    'Always can find a solution',
+    'High research skills'
+  ]
+},{
+  name: 'Hard',
+  list: [
+    'Web development',
+    'Using and writting REST API',
+    'Working with Responsive Web Design',
+    'Creating static and dynamic websites with use of the documentation and interactive documents'
+  ]
+}];
+
+export default data;
--- frontend/src/pages/landing-page/LandingPage.js
+++ frontend/src/pages/landing-page/LandingPage.js
@@ -1,5 +1,6 @@
 import Header from '../../components/header/Header';
 import Intro from '../../components/intro/Intro';
+import Skills from '../../components/skills/Skills';
 import Technologies from '../../components/technologies/Technologies';
 
 const LandingPage = () => {
@@ -8,6 +9,7 @@ const LandingPage = () => {
       <Header/>
       <main>
         <Intro id="aboutme" className="pt-10"/>
+        <Skills id="skills" className="pt-0"/>
         <Technologies id="technologies" className="pt-10"/>
       </main>
     </>
